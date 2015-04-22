d8519d1e9fa84321b2eecd5f6b73057918e088d5
monolok <antoinebertin@me.com>
2015-04-22 20:26:10 +0200

    routes done

---
+++ client/models.js
@@ -0,0 +1,23 @@
+/**
+* Models
+*/
+
+//Messages = new Meteor.Collection('messages');
+
+/**
+* Routes
+*/
+
+Router.configure({
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
+    layoutTemplate: 'layout'
+});
+
+Router.route('/', function() {
+	this.render('Index');
+});
+
+Router.route('/about');
+Router.route('/blog');
+Router.route('/contact');
